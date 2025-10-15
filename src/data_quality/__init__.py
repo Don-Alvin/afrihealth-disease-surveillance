79780a5cb3a476b82b5bc7dc2cd6cfc48f074a26
Don-Alvin <donalvin41@gmail.com>
2025-10-15 18:20:01 +0300

    fix: remove extra files

--- src/data_quality/__init__.py
+++ src/data_quality/__init__.py
@@ -2,6 +2,5 @@
 Data quality package
 """
 from .quality_checks import DataQualityChecker
-from .quality_report import generate_quality_report
 
-__all__ = ['DataQualityChecker', 'generate_quality_report']
+__all__ = ['DataQualityChecker']
--- src/data_quality/quality_report.py
+++
@@ -1,2 +0,0 @@
-def generate_quality_report(results_json_path: str, output_pdf_path: str):
-    pass
